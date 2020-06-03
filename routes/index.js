a2f51a1c0afe9920bd69fd1ba67c456281979fe7
ndhungw <ndh1379@gmail.com>
2020-06-03 11:46:20 +0700

    create partials

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* GET contact page. */
+router.get("/pages/contact.html", function (req, res, next) {
+  res.render("pages/contact.ejs", { title: "Express" });
+});
+
+/* GET single-page page. */
+router.get("/pages/single_page.html", function (req, res, next) {
+  res.render("pages/single-page.ejs", { title: "Express" });
+});
+
+/* GET single-page page. */
+router.get("/single_page.html", function (req, res, next) {
+  res.render("pages/single-page.ejs", { title: "Express" });
 });
 
 module.exports = router;
