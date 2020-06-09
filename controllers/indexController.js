6d126d8bf476a72bed54b9d99d3b0406e3153dce
ndhungw <ndh1379@gmail.com>
2020-06-09 21:10:48 +0700

    remove 'latest news' in home page and single article page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -7,14 +7,16 @@ indexController.showHomePage = async (req, res) => {
   try {
     console.log("... RUN - indexController.showHomePage");
     const categories = await CategoryModel.getAllCategories();
+
+    // build an array about each category contains it's articles
     let categoriesWithCorrespondingArticles = [];
 
     for (let i = 0; i < categories.length; i++) {
       const articles = await ArticleModel.getAllArticlesByCategory(
         categories[i].title
-      ).limit(5);
-      const latestArticle = articles[0];
-      let rightArticleList = [];
+      ).limit(5); // only get 5 articles to show
+      const latestArticle = articles[0]; // the first will be the latest cuz we had used find with sort desc
+      let rightArticleList = []; // articles to show on the right
 
       for (let j = 1; j < articles.length; j++) {
         rightArticleList[j - 1] = articles[j];
@@ -27,28 +29,19 @@ indexController.showHomePage = async (req, res) => {
       };
     }
 
-    console.log(categoriesWithCorrespondingArticles);
-
     res.render("default/index", {
       title: "Trang chủ",
       categoriesWithCorrespondingArticles: categoriesWithCorrespondingArticles,
     });
-
-    // if (categories != null && articles != null) {
-    //   res.render("default/index", {
-    //     title: "Trang chủ",
-    //     categoriesWithCorrespondingArticles: categoriesWithCorrespondingArticles,
-    //   });
-    // } else {
-    //   if (!categories) {
-    //     console.log("categories is null");
-    //   }
-    //   if (!articles) {
-    //     console.log("articles is null");
-    //   }
-    // }
   } catch (e) {
     console.log("... Get error when run - indexController.showHomePage - " + e);
+
+    if (!categories) {
+      console.log("categories is null");
+    }
+    if (!articles) {
+      console.log("articles is null");
+    }
   }
 };
 
