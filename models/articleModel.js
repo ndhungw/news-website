a6f5b595087a35f5aca6a6ee7dc63be206b3bf78
ndhungw <ndh1379@gmail.com>
2020-06-15 11:55:20 +0700

    add "upload cover for article" feature

--- models/articleModel.js
+++ models/articleModel.js
@@ -15,6 +15,14 @@ let articleSchema = new Schema({
   description: {
     type: String,
   },
+  coverImage: {
+    type: Buffer,
+    required: true,
+  },
+  coverImageType: {
+    type: String,
+    required: true,
+  },
   category: {
     type: String,
     required: true,
@@ -35,7 +43,6 @@ let articleSchema = new Schema({
   ctgSlug: {
     type: String,
     required: true,
-    unique: true,
   },
   sanitizedHTML: {
     type: String,
@@ -66,6 +73,12 @@ articleSchema.pre("validate", function (next) {
 
   next();
 });
+
+articleSchema.virtual('coverImagePath').get(function() {
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+  }
+})
 
 let ArticleModel = mongoose.model("Article", articleSchema, "articles");
 
